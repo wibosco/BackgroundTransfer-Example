c9af67db10b3e5f55cf05d1206516faccaebf306
William Boles <wibosco@users.noreply.github.com>
2018-04-28 10:12:42 +0100

    Initial project setup

---
+++ BackgroundTransfer-ExampleTests/BackgroundTransfer_ExampleTests.swift
@@ -0,0 +1,29 @@
+//
+//  BackgroundTransfer_ExampleTests.swift
+//  BackgroundTransfer-ExampleTests
+//
+//  Created by William Boles on 28/04/2018.
+//  Copyright © 2018 William Boles. All rights reserved.
+//
+
+import XCTest
+@testable import BackgroundTransfer_Example
+
+class BackgroundTransfer_ExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
